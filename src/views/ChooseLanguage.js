ce5af3dc5d9a9d1c4e3d9dc6dd6e28141e642c3d
Wong Jia Hau <hou32hou@gmail.com>
2018-03-21 16:22:32 +0800

    Added link to go to progress page

--- src/views/ChooseLanguage.js
+++ src/views/ChooseLanguage.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import Center from 'react-center';
+import {Link} from 'react-router-dom'
 import {Dialog} from './Dialog';
 
 export const ChooseLanguage = () => (
-    <Dialog
-        title="Choose a language"
-        word1="Chinese"
-        link1="/chinese"
-        word2="English"
-        link2="/english"
-    />
+    <div>
+        <Dialog
+            title="Choose a language"
+            word1="Chinese"
+            link1="/chinese"
+            word2="English"
+            link2="/english"
+        />
+        <Center>
+            <Link to="/progress">See progress</Link>
+        </Center>
+    </div>
     
 );

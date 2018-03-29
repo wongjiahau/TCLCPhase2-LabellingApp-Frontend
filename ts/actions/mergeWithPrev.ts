9edc4b509dba7303f2d0c8911236027dc2aed5ce
Wong Jia Hau <hou32hou@gmail.com>
2018-03-29 11:27:46 +0800

    Past test for mergeWithPrev

--- ts/actions/mergeWithPrev.ts
+++ ts/actions/mergeWithPrev.ts
@@ -1,9 +1,19 @@
-import { IPostListState, IPostListStateAction } from "./postListStateAction";
-export class MergeWithPrev implements IPostListStateAction {
-    public constructor(private targetIndex: number) {}
-    public run(state: IPostListState): IPostListState {
-        const toBeRemoved = state.postViewModels.splice(this.targetIndex, 1)[0];
-        state.postViewModels[this.targetIndex - 1].value += " " + toBeRemoved.value;
+import { Action } from "./action";
+import { IPostListState } from "./postListStateAction";
+export class MergeWithPrev extends Action<IPostListState> {
+    public constructor(private targetIndex: number) {
+        super();
+    }
+
+    protected modifyState(state: IPostListState): IPostListState {
+        const index =
+            this.targetIndex === -1 ?
+            state.currentFocusIndex :
+            this.targetIndex;
+
+        const toBeRemoved = state.postViewModels.splice(index, 1)[0];
+        state.postViewModels[index - 1].value += " " + toBeRemoved.value;
+        state.currentFocusIndex = index - 1;
         return state;
     }
 }
--- ts/actions/tests/mergeWithPrev.test.ts
+++ ts/actions/tests/mergeWithPrev.test.ts
@@ -24,11 +24,20 @@ describe("MergeWithPrev", () => {
     });
 
     it("should merge the current focused postViewModel if targetIndex = -1", () => {
-
+        const state = getMockState();
+        state.currentFocusIndex = 3;
+        const newState1 = new MergeWithPrev(-1).run(state);
+        const newState2 = new MergeWithPrev(3).run(state);
+        expect(newState1).to.deep.eq(newState2);
     });
 
     it("should change the focusIndex to the one that is merged into", () => {
-
+        const originalState = getMockState();
+        expect(originalState.currentFocusIndex).to.eq(0);
+        const targetIndex = 3;
+        const action = new MergeWithPrev(targetIndex);
+        const newState = action.run(clone(originalState));
+        expect(newState.currentFocusIndex).to.eq(targetIndex - 1);
     });
 
     it("should set the semantic_value according to the bottom one", () => {
